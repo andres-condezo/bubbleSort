58aa7bb9ede0cd832dfe895d786a0409fefd5246
adrs <ra.condezomonge@gmail.com>
2020-12-19 00:43:04 -0500

    init

---
+++ bubbleSort.py
@@ -0,0 +1,11 @@
+# 1.- Comenzar a hacer comparaciones de elementos adyacentes.
+# 2.- Repetir hasta tener una pasada completa sin tener ningun swap.
+
+def bubbleSort(array):
+  n = len(array)
+
+  for i in rangeñ(n)
+
+    for j in range(0, n-i-1):
+
+      if array[j] > array[j+1] :
